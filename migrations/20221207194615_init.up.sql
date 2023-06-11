0912d304138dd6f062286f22322940113d4bcf5b
Bechma <brunochenoll@hotmail.com>
2023-06-11 15:00:13 +0200

    format up migration

--- migrations/20221207194615_init.up.sql
+++ migrations/20221207194615_init.up.sql
@@ -5,13 +5,11 @@ CREATE TABLE IF NOT EXISTS Users (
     bio text NULL,
     image text NULL
 );
-
 CREATE TABLE IF NOT EXISTS Follows (
     follower text NOT NULL REFERENCES Users(username) ON DELETE CASCADE ON UPDATE CASCADE,
     influencer text NOT NULL REFERENCES Users(username) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (follower, influencer)
 );
-
 CREATE TABLE IF NOT EXISTS Articles (
     slug text NOT NULL PRIMARY KEY,
     author text NOT NULL REFERENCES Users(username) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -21,21 +19,17 @@ CREATE TABLE IF NOT EXISTS Articles (
     created_at TIMESTAMPTZ NOT NULL default NOW(),
     updated_at TIMESTAMPTZ NOT NULL default NOW()
 );
-
 CREATE TABLE IF NOT EXISTS ArticleTags (
     article text NOT NULL REFERENCES Articles(slug) ON DELETE CASCADE ON UPDATE CASCADE,
     tag text NOT NULL,
     PRIMARY KEY (article, tag)
 );
-
-CREATE INDEX tags ON ArticleTags (tag);
-
+CREATE INDEX IF NOT EXISTS tags ON ArticleTags (tag);
 CREATE TABLE IF NOT EXISTS FavArticles (
     article text NOT NULL REFERENCES Articles(slug) ON DELETE CASCADE ON UPDATE CASCADE,
     username text NOT NULL REFERENCES Users(username) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (article, username)
 );
-
 CREATE TABLE IF NOT EXISTS Comments (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     article text NOT NULL REFERENCES Articles(slug) ON DELETE CASCADE ON UPDATE CASCADE,
